110004b5ad64180af79e9954e7ed2d01fedcbe2d
Jonathana Kahana <jonkahana1996@gmail.com>
2021-12-27 01:47:47 +0200

    first commit after adding numpy datasets and setting up smallnorb, cars3d and shapes3d experiments

---
+++ bash_scripts/BBVAE/smallnorb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /cs/labs/yedid/jonkahana/lord_cl_venv/bin/activate.csh
+export CUDA_HOME="/usr/local/nvidia/cuda/11.0/"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CUDA_HOME/lib64:$CUDA_HOME/extras/CUPTI/lib64"
+
+cd /cs/labs/yedid/jonkahana/external/VAEs/PyTorch-VAE
+
+python -u run.py -c configs/BVAE/bbvae__smallnorb.yaml
